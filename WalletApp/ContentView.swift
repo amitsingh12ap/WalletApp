adb613f218705ff3f6bb0724b099d9c8fb6ba8fa
Amit Kr Singh <amit1.singh@africa.airtel.com>
2021-06-07 05:24:11 +0530

    Initial Commit

---
+++ WalletApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WalletApp
+//
+//  Created by 13216146 on 07/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
